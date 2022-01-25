20c879e7ef56e8d302e2303658b37067671f6cad
HakarAziz <hakaraziz1@gmail.com>
2022-01-25 14:30:46 +0100

    New Puzzle Files

---
+++ Assets/Scripts/PuzzelScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzelScript : MonoBehaviour
+{
+    public List<GameObject> Pieces = new List<GameObject>();
+    public List<Vector3> StartPos = new List<Vector3>();
+    public List<Quaternion> rot = new List<Quaternion>();
+
+    void Start()
+    {
+
+        AddPieceInformation();
+    }
+
+    void Update()
+    {
+
+
+
+
+    }
+
+    public void AddPieceInformation()
+    {
+        Transform[] Children = GetComponentsInChildren<Transform>();
+        foreach (Transform child in Children)
+        {
+
+            Pieces.Add(child.gameObject);
+            StartPos.Add(child.transform.position);
+            rot.Add(child.transform.rotation);
+        }
+    }
+
+    public void ChangePos(GameObject PieceName)
+    {
+        int Nr = Pieces.IndexOf(PieceName);
+
+        Destroy(Pieces[Nr]);
+
+        Pieces[Nr].transform.position = StartPos[Nr];
+        Pieces[Nr].transform.rotation = rot[Nr];
+
+        Instantiate(Pieces[Nr], Pieces[Nr].transform.position, Pieces[Nr].transform.rotation);
+
+
+    }
+
+
+}
---
+++ Assets/Scripts/Zone_Trigger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zone_Trigger : MonoBehaviour
+{
+    public Renderer rend;
+    public GameObject puzzle_piece;
+    public GameObject pie;
+    PuzzelScript puzzelScript;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*rend = GetComponent<Renderer>();
+        rend.enabled = false;   //Turns the visibility of the object off
+
+
+
+        puzzle_piece = GameObject.Find("Puzzle_0");  //Returns the gameobject in the scene
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        puzzelScript = pie.GetComponent<PuzzelScript>();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        string PieceName;
+        if (other.tag == gameObject.tag)  //Checks if the correct piece is in the correct spot and deletes the zone
+        {
+            print(gameObject.tag + ", Right piece");
+            Destroy(gameObject);
+        }
+        else
+        {
+            print("Wrong Piece");
+            puzzelScript.ChangePos(other.gameObject);
+        }
+
+    }
+}
