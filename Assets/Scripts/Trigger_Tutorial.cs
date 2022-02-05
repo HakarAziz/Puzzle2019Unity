66b27c808ab39332b8264311f5330e6caf4e9cde
JohanMat99 <johanmattsson99@live.se>
2022-02-05 12:53:16 +0100

    Skapat en Tutorial
    Saknar dock timer men har kommenterat där det behövs i koden.

---
+++ Assets/Scripts/Trigger_Tutorial.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger_Tutorial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject pie;
+    Tutorial tutorialScript;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tutorialScript = pie.GetComponent<Tutorial>();
+    }
+
+    void OnTriggerEnter(Collider Contact) 
+    {
+        if (Contact.tag == gameObject.tag) 
+        {
+
+            print("Dom krockade!!!!");
+            tutorialScript.Collading();
+        }
+        else 
+        { 
+        
+        }
+
+
+
+    }
+}
---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Timers;
+
+
+public class Tutorial : MonoBehaviour
+{
+  
+    public Text UpdateText;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        UpdateText.text = "Welcome to this tutorial !!";
+
+        //Timer som jag inte fattar hur man gör skall sitta på 2 eller 3 sek
+
+        UpdateText.text = "Try to grab the cube by squeezeing the cube with your thumb and index finger";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void GrabCube()
+    {
+        
+        UpdateText.text = "You managed to grab the cube !!";
+
+
+        //Timer som jag inte fattar hur man gör skall sitta på 2 eller 3 sek
+
+        UpdateText.text = "Now try to place the cube on the plate";
+
+
+    }
+
+    public void Collading() 
+    {
+        UpdateText.text = "You managed to drop the cube on the plate";
+
+        //Timer som jag inte fattar hur man gör skall sitta på 2 eller 3 sek
+
+        UpdateText.text = "You passed the tutorial !!";
+    }
+
+
+
+
+
+}
